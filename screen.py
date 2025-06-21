66cbc42671798f74dc33beb1e800852706779b60
Vasant <vasantdave2002@gmail.com>
2025-06-21 13:18:05 -0400

    Add function to close the window gracefully

--- screen.py
+++ screen.py
@@ -152,8 +152,6 @@ def main():
    display = Display()
    display.start()
      
-# run the main function only if this module is executed as the main script
-# (if you import this as a module then nothing is executed)
 if __name__=="__main__":
     # call the main function
     main()
